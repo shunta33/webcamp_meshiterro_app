b9d52917c8080f99a44055cba81a38cc246b6274
shunta33 <syuntaochiai33@gmail.com>
2020-12-16 15:22:56 +0000

    [Finish]4-3

--- meshiterro/config/initializers/application_controller_renderer.rb
+++ meshiterro/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '55dc2ab8d1953777d3b10572a58c9b7b1d7cc2d618b09926b3a2dd9cfad11670552907abbfa00e77c60767eb4e756ba5bf0da984d1775727936f32115f0bcd52'
